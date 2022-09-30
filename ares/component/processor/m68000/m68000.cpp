0de20a696e61799a64bcac8d6f751b5e9ff9511d
invertego <invertego@users.noreply.github.com>
2022-09-29 23:30:46 -0700

    update for new test suite
    https://github.com/TomHarte/ProcessorTests/tree/main/680x0/68000/v1

--- ares/component/processor/m68000/m68000.cpp
+++ ares/component/processor/m68000/m68000.cpp
@@ -102,14 +102,16 @@ struct test_state {
   u32 pc;
   u32 sr;
   u32 usp;
+  u32 ssp;
+  std::array<u32, 2> prefetch;
+  std::vector<std::array<u32, 2>> ram;
 };
 
 struct test_case {
   std::string name;
-  test_state initial_state;
-  std::vector<int> initial_memory;
-  test_state final_state;
-  std::vector<int> final_memory;
+  test_state initial;
+  test_state final;
+  u32 length;
 };
 
 void from_json(const json& j, test_state& s) {
@@ -120,7 +122,7 @@ void from_json(const json& j, test_state& s) {
   j.at("a4").get_to(s.a[4]);
   j.at("a5").get_to(s.a[5]);
   j.at("a6").get_to(s.a[6]);
-  j.at("a7").get_to(s.a[7]);
+  j.at("ssp").get_to(s.a[7]);
   j.at("d0").get_to(s.d[0]);
   j.at("d1").get_to(s.d[1]);
   j.at("d2").get_to(s.d[2]);
@@ -132,22 +134,24 @@ void from_json(const json& j, test_state& s) {
   j.at("pc").get_to(s.pc);
   j.at("sr").get_to(s.sr);
   j.at("usp").get_to(s.usp);
+  j.at("prefetch").get_to(s.prefetch);
+  j.at("ram").get_to(s.ram);
 }
 
 void from_json(const json& j, test_case& t) {
   if(j.empty()) return;
   j.at("name").get_to(t.name);
-  j.at("initial state").get_to(t.initial_state);
-  j.at("initial memory").get_to(t.initial_memory);
-  j.at("final state").get_to(t.final_state);
-  j.at("final memory").get_to(t.final_memory);
+  j.at("initial").get_to(t.initial);
+  j.at("final").get_to(t.final);
+  j.at("length").get_to(t.length);
 }
 
 struct CPUHarness : ares::M68000 {
+  u32 clock = 0;
   std::map<u32, u8> memory;
   CPUHarness();
-  auto idle(u32 clocks) -> void override {}
-  auto wait(u32 clocks) -> void override {}
+  auto idle(u32 clocks) -> void override { clock += clocks; }
+  auto wait(u32 clocks) -> void override { clock += clocks; }
   auto read(n1 upper, n1 lower, n24 address, n16 _ = 0) -> n16 override {
     n16 data;
     if(lower) data.byte(0) = memory[address | 1];
@@ -161,44 +165,55 @@ struct CPUHarness : ares::M68000 {
 };
 
 CPUHarness::CPUHarness() {
-  for(const auto& entry : std::filesystem::directory_iterator{ "." }) {
+  for(const auto& entry : std::filesystem::directory_iterator{ "../tests" }) {
     const auto& path = entry.path();
     if(path.extension() != ".json") continue;
-    //if(path.filename() != "addx_subx.json") continue;
+    //if(path.filename() != "ADD.l.json") continue;
+    u32 srMask = ~0u;
+    if(path.filename() == "ASR.b.json"
+    || path.filename() == "ASR.l.json"
+    || path.filename() == "ASR.w.json"
+    ) srMask &= ~0x11u; //bugged tests
+    print(path.filename().string().c_str(), "\n");
 
     std::ifstream f(path);
     json data = json::parse(f);
     auto tests = data.get<std::vector<test_case>>();
+    bool breakOnce = false;
 
     for(const auto& test : tests) {
       if(test.name.empty()) continue;
       //if(test.name != "ADD d108") continue;
       //if(test.name == "LINK.w 0007") continue;
+      if(test.final.pc == 0x1400) continue; //address errors nyi
 
       power();
-      const auto& is = test.initial_state;
+      const auto& is = test.initial;
       for(auto i : range(8)) {
         r.a[i] = is.a[i];
         r.d[i] = is.d[i];
       }
       r.sp = is.usp;
-      r.pc = is.pc;
+      r.pc = is.pc + 4;
       writeSR(is.sr);
 
       memory.clear();
-      const auto& im = test.initial_memory;
-      for(auto i : range(im.size() >> 1)) {
-        memory[im[i << 1 | 0]] = im[i << 1 | 1];
+      const auto& im = is.ram;
+      for(auto i : range(im.size())) {
+        memory[im[i][0]] = im[i][1];
       }
 
-      prefetch();
-      prefetch();
+      r.ir  = is.prefetch[0];
+      r.irc = is.prefetch[1];
 
+      clock = 0;
       instruction();
 
       bool any = false;
 
-      const auto& fs = test.final_state;
+      if(!r.s) swap(r.a[7], r.sp);
+
+      const auto& fs = test.final;
       for(auto i : range(8)) {
         if(r.a[i] != fs.a[i]) {
           print("a", i, ": ", hex(r.a[i], 8), ", ", hex(fs.a[i], 8), "\n"); any = true;
@@ -214,14 +229,23 @@ CPUHarness::CPUHarness() {
         print("pc: ", hex(r.pc, 8), ", ", hex(fs.pc, 8), "\n"); any = true;
       }
       auto sr = readSR();
-      if(sr != fs.sr) {
+      if((sr & srMask) != (fs.sr & srMask)) {
         print("sr: ", hex(sr, 8), ", ", hex(fs.sr, 8), "\n"); any = true;
       }
+      if(r.ir != fs.prefetch[0]) {
+        print("prefetch[0]: ", hex(r.ir, 8), ", ", hex(fs.prefetch[0], 8), "\n"); any = true;
+      }
+      if(r.irc != fs.prefetch[1]) {
+        print("prefetch[1]: ", hex(r.irc, 8), ", ", hex(fs.prefetch[1], 8), "\n"); any = true;
+      }
+      //if(clock != test.length) {
+        //print("length: ", hex(clock, 8), ", ", hex(test.length, 8), "\n"); any = true;
+      //}
 
-      const auto& fm = test.final_memory;
-      for(auto i : range(fm.size() >> 1)) {
-        int addr = fm[i << 1 | 0];
-        u8 data = fm[i << 1 | 1];
+      const auto& fm = fs.ram;
+      for(auto i : range(fm.size())) {
+        u32 addr = fm[i][0];
+        u8 data = fm[i][1];
         u8 actual = memory[addr];
         if(actual != data) {
           print(hex(addr, 8), ": ", hex(actual, 2), ", ", hex(data, 2), "\n"); any = true;
@@ -229,7 +253,11 @@ CPUHarness::CPUHarness() {
       }
 
       if(any) {
-        print(test.name.c_str(), " | ", disassembleInstruction(is.pc), "\n");
+        print(test.name.c_str(), /*" | ", disassembleInstruction(is.pc),*/ "\n");
+        if(!breakOnce) {
+          breakOnce = true;
+          break;
+        }
       }
     }
   }
